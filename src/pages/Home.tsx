a4566708c67524c5b565b580d02b54b88f81907b
Christian Andreas Heuer <christianheuer2000@gmail.com>
2025-02-07 21:38:04 +0100

    updated intro text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,13 +15,18 @@ export default function Home() {
             <p>
               Hello there!
               My name is Christian.
-              I am a software developer and BSc alumni in Computer Science from RWTH Aachen University.
+              I am a software developer and technology enthusiast.
             </p>
             <p>
-              
+              I have always been passionate about computers since a young age and have therefore honed my programming skills as a hobby while going to school.
+              Projects I have worked on include little games, websites, and tools.
             </p>
             <p>
-              Feel free to take a look at my past projects on this portfolio website of mine.
+              Due to my passion for technology, I decided to study Computer Science at RWTH Aachen University, where I have learned a lot about the theoretical and practical aspects of computer science.
+              Now I have graduated as a Bachelor of Science in Computer Science and am looking for new opportunities to grow and learn.
+            </p>
+            <p>
+              Please feel free to take a look at my past projects on this portfolio website of mine.
             </p>
           </div>
           <div className="link-btns">
